e28be8168499303f90a582188b179ee46ed2c05b
xmzhoudev1997 <165015145+xmzhoudev1997@users.noreply.github.com>
2024-04-01 10:03:57 +0800

    修改文档生成位置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: 'rc-tabs',
   themeConfig: {
     name: 'rc-tabs',
   },
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/rc-tabs/docs-dist/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/rc-tabs/rc-tabs/',
   hash: true,
   history: {type: 'hash',},
 });
